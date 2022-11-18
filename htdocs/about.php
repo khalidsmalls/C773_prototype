4998f1a8f71a93d51ff84eb9779b99867afdd576
khalid <khalidsmalls@protonmail.com>
2022-11-17 23:02:42 -0500

    about page is at a decent place. the content needs work, but the boxes are in the right places at small, medium, and large screen sizes.

---
+++ htdocs/about.php
@@ -0,0 +1,68 @@
+<?php 
+	$page_title = 'About Us';
+	include('includes/header.html');
+?> 
+	<!-- Begin page content -->
+
+
+	<div class="d-flex justify-content-center">
+		<h1>About Us</h1>
+	</div>
+	<hr/>
+
+	<div class="d-flex justify-content-center">
+		<section class="d-lg-flex page-content mb-2 p-1 col-lg-9">
+			<div class="row">
+				<picture class="col-lg-5">
+					<source media="(min-width: 600px)" srcset="../assets/images/brown-dog-med.jpg">
+					<source media="(min-width: 768px)" srcset="../assets/images/brown-dog-large.jpg">
+					<img class="img-fluid" src="../assets/images/brown-dog-small.jpg" alt="A cute brown dog" style="height:100%;">
+				</picture>
+				<div class="col-lg-6" style="align-self:center;">
+					<p class="p-1">Pets are nature's gift to humanity. It has been scientifically proven that opening our homes and hearts to a pet increases our longevity and improves our overall quality of life as well as the lives of our pets. At Pet Paradigm Professionals, our mission is to offer resources to help you care for your furry, scaly, feathery, and slimy loved ones. Our pet experts—or “Pexperts”—have been working with pet owners and professionals alike for the past twelve years. They offer <a href="#">one-on-one consultations</a> with current and prospective pet owners as well as group presentations designed for veterinary, pet shelter, and pet breeding professionals.</p>
+					<div>
+						<button class="btn" id="cta-button">
+							Schedule a one-on-one Consultation 
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<div class="d-flex justify-content-center">
+		<div class="row col-xs-12 col-lg-9"> 
+			<div class="col-xs-12 col-md-6 mb-2 ps-lg-0">
+				<section class="page-content p-2 mb-2">
+					<h2 class="p-1">Evidence-based pet care advice for every pet lover</h2>
+						<picture>
+							<source media="(min-width: 600px)" srcset="../assets/images/veterinary-med.jpg">
+							<source media="(min-width: 768px)" srcset="../assets/images/veterinary-large.jpg">
+							<img class="img-fluid" id="hero-image" src="../assets/images/veterinary-small.jpg" alt="A veterinarian caring for a dog">
+						</picture>
+					<p class="p-1">Looking for basic pet care advice for the most common type of pets? Need additional help determining which type of pet is right for you and your family? We will work with you and provide tailored evidence-based pet care to ensure lifelong health and wellness of your new companion. </p>
+				</section>
+			</div>
+			<div class="col-xs-12 col-md-6 mb-2 pe-lg-0 pb-2">
+				<section class="page-content mb-2" style="height:100%;">
+					<h2 class="pt-2 ps-4">Services</h2>
+					<hr/>
+
+					<ul class="ps-2 pb-1">
+						<li class="mb-3" style="line-height:1.3;">Virtual consultations and presentations</li>
+						<li class="mb-3" style="line-height:1.3;">One-on-one consultations concerning individual pets</li>
+						<li class="mb-3" style="line-height:1.3;">Group presentations regarding general pet care (can be tailored to the needs of the group)</li>
+					</ul>
+					<h4>We are also happy to help you navigate the adoption process!</h4>
+					<p class="p-1">We will guide you through each step in the process from determining which type of pet is best suited for your family and home environment, to completing the necessary paperwork, to bringing your new loved one home. Please fill out our contact form to request a consultation. We will contact you within 48 hours to schedule a consultation. All fields are required.</p>
+				</section>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!-- end page content --> 
+<?php
+	include('includes/footer.html'); 	
+?>
--- htdocs/index.php
+++ htdocs/index.php
@@ -5,12 +5,12 @@
 	<!-- Begin page content -->
 
 	<section class="d-flex justify-content-center py-2">
-		<div id="hero-image-div" class="col-xs-12 col-md-10 col-lg-9">
+		<div id="hero-image-div" class="col-xs-12 col-md-12 col-lg-9">
 			<div class="p-1">
 				<picture>
 					<source media="(min-width: 600px)" srcset="../assets/images/dog-and-cat-med.jpg">
 					<source media="(min-width: 768px)" srcset="../assets/images/dog-and-cat-desktop.jpg"> 
-					<img class="img-fluid" id="hero-image" src="../assets/images/dog-and-cat-mobile.jpg" alt="">
+					<img class="img-fluid" id="hero-image" src="../assets/images/dog-and-cat-mobile.jpg" alt="A dog and cat lying together">
 				</picture>
 			</div>
 			<div class="p-1" id="mission-statement">
@@ -30,20 +30,27 @@
 	</section> 
 
 	<div class="d-flex justify-content-center">
-		<div class="row col-xs-12 col-lg-9"> <!-- px-2 -->
+		<div class="row col-xs-12 col-lg-9"> 
 			<div class="col-xs-12 col-md-6 mb-2 ps-lg-0">
 				<section class="page-content mb-2 p-2">
 					<h1>Evidence-based pet care advice for every pet lover</h1>
-					<p class="p-1">Pets are nature's gift to humanity. It has been scientifically proven that opening our homes and hearts to a pet increases our longevity and improves our overall quality of life as well as the lives of our pets. Our pet experts—or “Pexperts”—have been working with pet owners and professionals alike for the past twelve years. They offer <a href="#">one-on-one consultations</a> with current and prospective pet owners as well as group presentations designed for veterinary, pet shelter, and pet breeding professionals.</p>
+					<p class="p-1">Looking for basic pet care advice for the most common type of pets? Need additional help determining which type of pet is right for you and your family? We will work with you and provide tailored evidence-based pet care to ensure lifelong health and wellness of your new companion. </p>
 				</section>
 				<section class="page-content p-2">
-					<p class="p-1">Looking for basic pet care advice for the most common type of pets? Need additional help determining which type of pet is right for you and your family? We will work with you and provide tailored evidence-based pet care to ensure lifelong health and wellness of your new companion. </p>
+					<h2 style="padding-left:32px;">Services</h2>
+					<hr/>
+					<ul class="p-0">
+						<li class="mb-3" style="line-height:1.3; font-size:1.2em;">Virtual consultations and presentations</li>
+						<li class="mb-3" style="line-height:1.3; font-size:1.2em;">One-on-one consultations concerning individual pets</li>
+						<li class="mb-3" style="line-height:1.3; font-size:1.2em;">Group presentations regarding general pet care (can be tailored to the needs of the group)</li>
+					</ul>
 				</section>
 			</div>
 			<div class="col-xs-12 col-md-6 mb-2 pe-lg-0">
 				<aside class="page-content p-2" style="height:100%;">
 					<p class="p-1">We are also happy to help you navigate the adoption process! We will guide you through each step in the process from determining which type of pet is best suited for your family and home environment, to completing the necessary paperwork, to bringing your new loved one home. Please fill out our <a href="#">contact form</a> to request a consultation. We will contact you within 48 hours to schedule a consultation. All fields are required.</p>
-					<p>Looking for your new best friend? The following organizations can help you find and adopt the perfect companion:</p>
+					<h3>Looking for your new best friend?</h3>
+					<p>The following organizations can help you find and adopt the perfect companion:</p>
 					<div class="row">
 						<span class="col-md-4 col-lg-5 external-links">General Adoption Resources:</span>
 						<a href="https://theshelterpetproject.org/" target="_blank" class="col-md-4 external-links">https://theshelterpetproject.org/</a>
